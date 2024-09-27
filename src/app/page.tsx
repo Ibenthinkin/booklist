ce690931abcd0b8a0384dd26c57ae6a09deacbde
Ben <benjamin.reilly@gmail.com>
2024-09-27 17:36:56 -0400

    displayed some mock data. obviosuly this assignment is incomplete as of now but this was the only time i've had available this week.  I will work on it a bit more tomorrow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,68 @@
 export default function Home() {
+  interface Book {
+    id: string;
+    title: string;
+    author: string;
+    genre: string;
+    read: boolean;
+  }
+
+  // const books: Book[] = [];
+
+  const books = [
+    {
+      id: "bi4u3fo34278gf",
+      title: "Gravity's Rainbow",
+      author: "Thomas Pynchon",
+      genre: "Post Modern Novel",
+      read: true,
+    },
+    {
+      id: "bgn9845hg93bvf",
+      title: "Vineland",
+      author: "Thomas Pynchon",
+      genre: "Post Modern Novel",
+      read: false,
+    },
+    {
+      id: "gn3uov7584vb40",
+      title: "White Noise",
+      author: "Don Delilo",
+      genre: "Post Modern Novel",
+      read: false,
+    },
+    {
+      id: "fnire04j98287b",
+      title: "Cosmopolis",
+      author: "Don Delilo",
+      genre: "Post Modern Novel",
+      read: false,
+    },
+    {
+      id: "t348902hbvyub",
+      title: "Infinite Jest",
+      author: "David Foster Wallace",
+      genre: "Post Modern Novel",
+      read: true,
+    },
+  ];
+
   return (
     <main className="booklist">
       <h1>Booklist</h1>
-      <div className="">Add Book Component</div>
-      <div className="">Filter by Read Component</div>
-      <div className="">
-        list of books
-        <ul>
-          <li>Book 1</li>
-          <li>Book 2</li>
-          <li>Book 3</li>
-          <li>Book 4</li>
-        </ul>
+      <div className="p-4">Add Book Component</div>
+      <div className="p-4">Filter by Read Component</div>
+      <div className="container mx-auto">
+        <h3>list of books</h3>
+
+        {books.map((book) => (
+          <div key={book.id} className="card p-4 border-2 rounded">
+            <h3>{book.title}</h3>
+            <p>{book.author}</p>
+            <p>{book.genre}</p>
+            <p>{book.read ? "Read" : "Not Read"}</p>
+          </div>
+        ))}
       </div>
       <div className=""></div>
     </main>
